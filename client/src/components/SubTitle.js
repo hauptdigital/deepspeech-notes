618741a266e9c30feb6d0db094909e292f6ef409
hauptdigital <marc@haupt.digital>
2020-04-12 12:11:32 +0200

    Add title components to storybook and remove preset button story

---
+++ client/src/components/SubTitle.js
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+
+const SubTitle = styled.h2`
+  font-family: Montserrat;
+  font-size: 20px;
+  color: ${(props) => props.theme.colors.primary};
+`;
+
+export default SubTitle;
---
+++ client/src/components/Title.js
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+
+const Title = styled.h1`
+  font-family: Montserrat;
+  font-size: 48px;
+  color: ${(props) => props.theme.colors.secondary};
+`;
+
+export default Title;
