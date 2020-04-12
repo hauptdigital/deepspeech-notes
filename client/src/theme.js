5eecfe57b4ef0d1ad885727742bc1890cf914c41
Marc <marc@haupt.digital>
2020-04-12 11:28:29 +0200

    Install frontend dependencies on client (#16)
    * Create react app in client folder
    
    * Remove not needed 'create react app' files and not needed dependencies
    
    * Update gitignore
    
    * Remove yarn.lock to only use npm package-lock.json from now on
    
    * Install @emotion/core and styled
    
    * Install react-router-dom
    
    * Install prop-types
    
    * Initialize storybook
    
    * Create development and production scripts (#17)
    
    * Add hello world application in express
    
    * Change express server to run on port 8080
    
    * Update gitignore with build and dependency folder
    
    * Install concurrently and nodemon and add dev and prod scripts
    
    * Change entry point to server.js
    
    * Add proxy to redirect unknown requests to express server
    
    * Remove prod script
    
    * Add standard theme colors and globalstyles (#18)
    
    * Add and apply globalstyles
    
    * Install emotion theming and set colors in standard theme

---
+++ client/src/theme.js
@@ -0,0 +1,12 @@
+const theme = {
+  colors: {
+    background: '#4F4270',
+    primary: '#FFFFFF',
+    secondary: '#FBC968',
+    light: '#A29CB3',
+    altOne: '#FA8099',
+    altTwo: '#F4EDDA',
+  },
+};
+
+export default theme;
