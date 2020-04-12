1a0537236881c717901b3e4a2aded8e74d071989
hauptdigital <marc@haupt.digital>
2020-04-12 22:42:05 +0200

    Return React production build when in node production environment

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
+require('dotenv').config();
 const express = require('express');
+const path = require('path');
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
+  // Handle React routing, return all requests to React app
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
