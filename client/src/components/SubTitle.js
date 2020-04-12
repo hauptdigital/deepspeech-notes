b0841fa78bd7e90407a830936ffb270f041888f7
hauptdigital <marc@haupt.digital>
2020-04-12 18:16:30 +0200

    Remove not needed font-family style

--- client/src/components/SubTitle.js
+++ client/src/components/SubTitle.js
@@ -1,7 +1,6 @@
 import styled from '@emotion/styled';
 
 const SubTitle = styled.h2`
-  font-family: Montserrat;
   font-size: 20px;
   color: ${(props) => props.theme.colors.primary};
 `;
--- client/src/components/Title.js
+++ client/src/components/Title.js
@@ -1,7 +1,6 @@
 import styled from '@emotion/styled';
 
 const Title = styled.h1`
-  font-family: Montserrat;
   font-size: 48px;
   color: ${(props) => props.theme.colors.secondary};
 `;
