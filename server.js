a59d8e0f5aabbf7ab36034631b998ee771df3944
hauptdigital <marc@haupt.digital>
2020-04-13 15:19:57 +0200

    Update environment example file and add storybook route to express server

--- server.js
+++ server.js
@@ -7,10 +7,16 @@ const port = process.env.PORT || 8080;
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
   app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.join(__dirname, 'client/storybook-static')));
 
   // Handle React routing, return all requests to React app
-  app.get('*', (req, res) => {
+  app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+
+  // Setup Handle React routing, return all requests to React app
+  app.get('/storybook', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/storybook-static', 'index.html'));
   });
 }
 
