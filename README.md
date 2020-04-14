2ffd1c739abfa90e5b0a782c1843f10fc2119ecc
Marc <marc@haupt.digital>
2020-04-14 14:19:15 +0200

    Deployment (#21)
    * Move emotion-theming to client package.json
    
    * Install dotenv
    
    * Add preset-create-react-app as this is needed for building storybook in production
    
    * Return React production build when in node production environment
    
    * Remove not needed production scripts
    
    * Update environment example file and add storybook route to express server
    
    * Update readme with deployment instructions
    
    * Remove node environment from environment example file

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # deepspeech-notes
+
+## Deployment
+
+To deploy this application, your production environment must meet the following requirements:
+
+1. node.js
+2. npm
+
+After you moved the repository content to your webspace, you need to run these preconfigured scripts from the repository root directory:
+
+1. `npm prod-prebuild`
+2. `npm prod-build`
+3. Rename `.env.example` into `.env` and change content to desired port
+
+The `Express` server will handle the following requests:
+
+1. `https://your-url.com/storybook` will route to the storybook build
+2. All other requests (including `https://your-url.com`) will route to the React application build (= `"client/build"`)
--- server.js
+++ server.js
@@ -1,8 +1,24 @@
+require('dotenv').config();
 const express = require('express');
+const path = require('path');
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.join(__dirname, 'client/storybook-static')));
+
+  // Handle React routing, return all requests to React app
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+
+  // Setup Handle React routing, return all requests to React app
+  app.get('/storybook', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/storybook-static', 'index.html'));
+  });
+}
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
