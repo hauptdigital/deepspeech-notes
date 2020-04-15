f4d7e174619392c8d29da3a5e3a7f76c061a463d
hauptdigital <marc@haupt.digital>
2020-04-15 15:53:51 +0200

    Connect to socket in client app

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,12 @@ import React from 'react';
 import GlobalStyles from './GlobalStyles';
 import { ThemeProvider } from 'emotion-theming';
 import theme from './theme';
+import io from 'socket.io-client';
+
+const SocketPort = process.env.SOCKET_PORT || 4000;
 
 function App() {
+  io('http://localhost:' + SocketPort);
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
