fef80f2ee8738203a02bbd3f9448d45a94853799
hauptdigital <marc@haupt.digital>
2020-04-16 23:15:20 +0200

    Create About, Note and Statistics page

---
+++ client/src/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Container from '../components/Container';
+import Title from '../components/Title';
+
+function Note() {
+  return (
+    <Container>
+      <Title>About</Title>
+    </Container>
+  );
+}
+
+export default Note;
---
+++ client/src/pages/Statistics.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Container from '../components/Container';
+import Title from '../components/Title';
+
+function Note() {
+  return (
+    <Container>
+      <Title>Statistics</Title>
+    </Container>
+  );
+}
+
+export default Note;
