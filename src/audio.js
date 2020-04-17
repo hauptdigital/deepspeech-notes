f93032acf13e21df11f17e13e35c60c15bfc68f2
Marc <marc@haupt.digital>
2020-04-17 11:45:04 +0200

    Add new VAD object and process read stream from demo audio file (#29)
    * Add new VAD object and process read stream from demo audio file
    
    * Add process voice functionality using deepspeech model and return text in console
    
    * Add demo file

--- src/audio.js
+++ src/audio.js
@@ -1,27 +1,39 @@
 const VAD = require('node-vad');
-const fs = require('fs');
 
-const vad = new VAD(VAD.Mode.VERY_AGGRESSIVE);
+const voiceActivityDetection = new VAD(VAD.Mode.VERY_AGGRESSIVE);
 
-const stream = fs.createReadStream('demo_pcm_s16_16000.raw');
-stream.on('data', (chunk) => {
-  vad
-    .processAudio(chunk, 16000)
-    .then((res) => {
-      switch (res) {
-        case VAD.Event.ERROR:
-          console.log('ERROR');
-          break;
-        case VAD.Event.NOISE:
-          console.log('NOISE');
-          break;
-        case VAD.Event.SILENCE:
-          console.log('SILENCE');
-          break;
-        case VAD.Event.VOICE:
-          console.log('VOICE');
-          break;
-      }
-    })
-    .catch(console.error);
-});
+function processAudioStream(audioStream, model, voiceActivityDetection) {
+  audioStream.on('data', (audio) => {
+    voiceActivityDetection
+      .processAudio(audio, 16000)
+      .then((res) => {
+        switch (res) {
+          case VAD.Event.ERROR:
+            console.log('ERROR');
+            break;
+          case VAD.Event.NOISE:
+            console.log('NOISE');
+            break;
+          case VAD.Event.SILENCE:
+            console.log('SILENCE');
+            break;
+          case VAD.Event.VOICE:
+            console.log('VOICE: ' + processVoice(audio, model));
+            break;
+        }
+      })
+      .catch(console.error);
+  });
+}
+
+function processVoice(audio, model) {
+  let modelStream = model.createStream();
+  model.feedAudioContent(modelStream, audio.slice(0, audio.length / 2));
+  let text = model.finishStream(modelStream);
+  return text;
+}
+
+module.exports = {
+  processAudioStream,
+  voiceActivityDetection,
+};
