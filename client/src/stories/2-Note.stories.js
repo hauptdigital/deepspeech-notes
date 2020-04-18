a078086dd25a8ba30163efe98a5011b63fe834f2
hauptdigital <marc@haupt.digital>
2020-04-18 20:54:29 +0200

    Install storybook viewport

--- client/src/stories/2-Note.stories.js
+++ client/src/stories/2-Note.stories.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import RecordButton from '../components/RecordButton';
+import PropTypes from 'prop-types';
 
 export default {
   title: 'Record Button',
   component: RecordButton,
 };
 
-export const RecordNote = () => <RecordButton />;
+export const Default = () => <RecordButton isRecording={false} />;
+export const Active = () => <RecordButton isRecording={true} />;
+
+RecordButton.propTypes = {
+  isRecording: PropTypes.bool.isRequired,
+  onClick: PropTypes.func,
+};
