4cd4846f366806be4c0592f8c81e3b22ca6d7fb4
hauptdigital <marc@haupt.digital>
2020-04-21 11:45:36 +0200

    Add backend audio processing, socket interaction and text-to-speech language model processing

--- server.js
+++ server.js
@@ -2,41 +2,19 @@ require('dotenv').config();
 const http = require('http');
 const express = require('express');
 const path = require('path');
+const { initSocket } = require('./src/socket');
+
 const port = process.env.PORT || 8080;
 const socketPort = process.env.SOCKET_PORT || 4000;
-const { createModel } = require('./src/model');
-const fs = require('fs');
-const { processAudioStream, voiceActivityDetection } = require('./src/audio');
-const { startSocket } = require('./src/socket');
 
 const app = express();
-
-// Language Model
-
-const modelDirectory = './src/model';
-
-const modelOptions = {
-  // The beam width used by the decoder. A larger beam width generates better results at the cost of decoding time.
-  BEAM_WIDTH: 1024,
-  // The alpha hyperparameter of the CTC decoder. Language Model weight.
-  LM_ALPHA: 0.75,
-  // The beta hyperparameter of the CTC decoder. Word insertion weight.
-  LM_BETA: 1.85,
-};
-
-const model = createModel(modelDirectory, modelOptions);
-
-// Process audio demo file
-
-const audioStream = fs.createReadStream('./src/demo_pcm_s16_16000.raw');
-processAudioStream(audioStream, model, voiceActivityDetection);
 
 if (process.env.NODE_ENV === 'production') {
   // Web microphone socket
 
   const server = http.createServer(app);
 
-  startSocket(server);
+  initSocket(server);
 
   // Serve any static files
   app.use(express.static(path.join(__dirname, 'client/build')));
@@ -68,7 +46,7 @@ if (process.env.NODE_ENV === 'production') {
     console.log(`SocketIO listening at http://localhost:${socketPort}`);
   });
 
-  startSocket(socket);
+  initSocket(socket);
 
   app.listen(port, () =>
     console.log(`Express server app listening at http://localhost:${port}`)
--- src/socket.js
+++ src/socket.js
@@ -1,10 +1,34 @@
 const io = require('socket.io');
+const {
+  createStream,
+  processAudioStream,
+  endAudioStream,
+  resetAudioStream,
+} = require('./audio.js');
 
-function startSocket(server) {
+function initSocket(server) {
   const socket = io(server, {});
 
   socket.on('connection', function (socket) {
     console.log('client connected');
+
+    createStream();
+
+    socket.on('stream-data', function (data) {
+      processAudioStream(data, (results) => {
+        socket.emit('recognize', results);
+      });
+    });
+
+    socket.on('stream-end', function () {
+      endAudioStream((results) => {
+        socket.emit('recognize', results);
+      });
+    });
+
+    socket.on('stream-reset', function () {
+      resetAudioStream();
+    });
 
     socket.once('disconnect', () => {
       console.log('client disconnected');
@@ -13,5 +37,5 @@ function startSocket(server) {
 }
 
 module.exports = {
-  startSocket,
+  initSocket,
 };
