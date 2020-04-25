c9878fa102bdf691664979a47d0d8e018c43b84e
hauptdigital <marc@haupt.digital>
2020-04-25 21:58:35 +0200

    Move fade in animation styles to styled component

--- client/src/components/TextChunk.js
+++ client/src/components/TextChunk.js
@@ -2,6 +2,22 @@ import styled from '@emotion/styled';
 
 const TextChunk = styled.span`
   ${(props) => (props.isNew ? '' : 'opacity: 0.5;')}
+  /* fadeIn animation */
+
+  &.fadeIn-enter,
+  &.fadeIn-appear {
+    opacity: 0.01;
+    position: relative;
+    bottom: 20px;
+  }
+
+  &.fadeIn-enter-active,
+  &.fadeIn-appear-active {
+    opacity: 1;
+    transition: all 300ms ease-in;
+    position: relative;
+    bottom: 0px;
+  }
 `;
 
 export default TextChunk;
