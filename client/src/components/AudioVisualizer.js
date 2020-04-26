d675c9d1a5692ff6eb379f4970c15102459e8784
hauptdigital <marc@haupt.digital>
2020-04-26 22:38:07 +0200

    Add rounded double ended visualization style and apply theme colors

--- client/src/components/AudioVisualizer.js
+++ client/src/components/AudioVisualizer.js
@@ -1,6 +1,7 @@
 import React, { useRef } from 'react';
 import PropTypes from 'prop-types';
 import styled from '@emotion/styled';
+import theme from '../theme';
 import { audioContext, mediaStreamSource } from '../utils/audio';
 
 const AudioWaveForm = styled.canvas`
@@ -25,7 +26,7 @@ function AudioVisualizer(props) {
 
     audioAnalyser.getByteFrequencyData(dataArray);
 
-    canvasContext.fillStyle = 'rgb(0, 0, 0)';
+    canvasContext.fillStyle = theme.colors.background;
     canvasContext.fillRect(0, 0, width, height);
 
     const barWidth = (width / bufferLength) * 2.5;
@@ -35,10 +36,39 @@ function AudioVisualizer(props) {
     for (let i = 0; i < bufferLength; i++) {
       barHeight = dataArray[i] / 2;
 
-      canvasContext.fillStyle = 'rgb(' + (barHeight + 100) + ',50,50)';
-      canvasContext.fillRect(x, height - barHeight / 2, barWidth, barHeight);
+      // Draw bar top
+      //canvasContext.fillStyle = 'rgb(' + (barHeight + 100) + ',50,50)';
+      canvasContext.fillStyle = theme.colors.secondary;
+      canvasContext.fillRect(
+        x,
+        height / 2 - barHeight / 2,
+        barWidth,
+        barHeight
+      );
 
-      x += barWidth + 1;
+      // Draw rounded end top
+      canvasContext.beginPath();
+      canvasContext.arc(
+        x + barWidth / 2,
+        height / 2 - barHeight / 2,
+        barWidth / 2,
+        0,
+        2 * Math.PI
+      );
+      canvasContext.fill();
+
+      // Draw rounded end bottom
+      canvasContext.beginPath();
+      canvasContext.arc(
+        x + barWidth / 2,
+        height / 2 + barHeight / 2,
+        barWidth / 2,
+        0,
+        2 * Math.PI
+      );
+      canvasContext.fill();
+
+      x += barWidth + 20;
     }
   }
 
@@ -54,6 +84,8 @@ function AudioVisualizer(props) {
       dataArray = new Uint8Array(bufferLength);
 
       canvasContext = AudioWaveFormCanvas.getContext('2d');
+      AudioWaveFormCanvas.width = AudioWaveFormCanvas.offsetWidth;
+      AudioWaveFormCanvas.height = AudioWaveFormCanvas.offsetHeight;
       width = AudioWaveFormCanvas.offsetWidth;
       height = AudioWaveFormCanvas.offsetHeight;
       console.log(width);
