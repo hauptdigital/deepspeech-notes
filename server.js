e3a42eac9c2e018911adf4f7cfe20491a1bdd897
hauptdigital <marc@haupt.digital>
2020-05-07 16:14:53 +0200

    Fix express server route

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/storybook-static')));
 
   // Handle React routing, return all requests to React app
-  app.get('/', (req, res) => {
+  app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 
